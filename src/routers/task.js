4ff72d9a14a01de0a32d6248d1dd73cb9d73114f
rehmatkarim <p146120@gmail.com>
2021-04-05 16:50:13 -0400

    added middleware for password hashing

--- src/routers/task.js
+++ src/routers/task.js
@@ -48,10 +48,12 @@ router.patch("/tasks/:id", async (req, res) => {
     return res.status(400).send({ error: "Invalid Operation" });
   }
   try {
-    const task = await Task.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true,
+    const task = await Task.findById(_id);
+    updates.forEach((update) => {
+      task[update] = req.body[update];
     });
+    await task.save();
+
     if (!task) {
       return res.status(404).send();
     }
