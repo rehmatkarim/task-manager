e96df970dbb5bdc95d491ba609f3c67a1f742425
rehmatkarim <p146120@gmail.com>
2021-04-05 15:16:51 -0400

    resource delete endpoints are added

--- src/index.js
+++ src/index.js
@@ -67,6 +67,19 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+app.delete("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findByIdAndDelete(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
@@ -124,6 +137,20 @@ app.patch("/tasks/:id", async (req, res) => {
     res.status(400).send(e);
   }
 });
+
+app.delete("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findByIdAndDelete(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
