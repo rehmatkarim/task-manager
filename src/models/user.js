80f6353f99294e550e34f01f437e11e942cf9015
rehmatkarim <p146120@gmail.com>
2021-04-06 16:38:28 -0400

    cascade delete tasks added

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("./task");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -44,7 +45,7 @@ const userSchema = new mongoose.Schema({
     {
       token: {
         type: String,
-        required: true, 
+        required: true,
       },
     },
   ],
@@ -90,6 +91,14 @@ userSchema.pre("save", async function (next) {
   if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+
+  next();
+});
+
+//Delete user tasks when user is removed
+userSchema.pre("remove", async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
 
   next();
 });
