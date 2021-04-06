e6a89f699b8130b6e7c65aac4316a33380463238
rehmatkarim <p146120@gmail.com>
2021-04-06 10:32:43 -0400

    login routes and jwt set

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -9,6 +10,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -38,18 +40,45 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  tokens:[{
+    token:{
+      type:String,
+      required:true
+    }
+  }]
 });
 
-userSchema.pre('save',async function(next){
-  const user =this;
-  
-  if (user.isModified('password')){
-    user.password = await bcrypt.hash(user.password,8)
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "hellothisisprac");
+  user.tokens = user.tokens.concat({token});
+  await user.save();
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+  return user;
+};
+
+//Hash the plantext password
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
   }
 
   next();
-
-})
-const User = mongoose.model("User",userSchema);
+});
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
