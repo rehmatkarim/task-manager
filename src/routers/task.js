b77131da2f5bbcb0709ffa78db8483d09c3f6b05
rehmatkarim <p146120@gmail.com>
2021-04-07 10:21:36 -0400

    pagination and filtering of tasks is added

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,8 +19,21 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
   try {
-    await req.user.populate("tasks").execPopulate();
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+        },
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send(e);
@@ -67,11 +80,10 @@ router.patch("/tasks/:id", auth, async (req, res) => {
   }
 });
 
-router.delete("/tasks/:id",auth, async (req, res) => {
+router.delete("/tasks/:id", auth, async (req, res) => {
   const _id = req.params.id;
   try {
-    
-    const task = await Task.findOneAndDelete({_id,owner:req.user._id});
+    const task = await Task.findOneAndDelete({ _id, owner: req.user._id });
     if (!task) {
       return res.status(404).send();
     }
